6254a727f1fc6adcb97db7673d95084035301b77
Leonid Rozenberg <leonid.rozenberg@digitalasset.com>
2020-06-12 10:46:17 -0400

    Correct the default newValue in UI dialog

--- ui/src/pages/report/Report.tsx
+++ ui/src/pages/report/Report.tsx
@@ -14,7 +14,7 @@ import useStyles from "./styles";
 export default function Report() {
   const classes = useStyles();
 
-  const defaultGive = { newOwner : " " };
+  const defaultGive = { newOwner : "" };
   const defaultGiveProps : InputDialogProps<Give> = {
     open:false,
     title: "Give Asset",
